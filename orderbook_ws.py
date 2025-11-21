4ad8459527f5113dcf3daf0a6dca8554c84747b7
nkrissetiyanto-png <nkrissetiyanto@me.com>
2025-11-22 01:11:13 +0700

    Update orderbook_ws.py

--- orderbook_ws.py
+++ orderbook_ws.py
@@ -5,9 +5,14 @@ import pandas as pd
 
 class OrderbookWebSocket:
     def __init__(self, symbol="btcusdt"):
-        self.url = f"wss://data-stream.binance.vision/ws/{symbol}@kline_1s"
+        self.url = f"wss://testnet.binance.vision/ws/btcusdt@kline_1s"
         self.bids = pd.DataFrame(columns=["price", "qty"])
         self.asks = pd.DataFrame(columns=["price", "qty"])
+
+    #"wss://stream.binance.com:9443/ws/btcusdt@kline_1s",
+    #"wss://data-stream.binance.vision/ws/btcusdt@kline_1s",
+    #"wss://testnet.binance.vision/ws/btcusdt@kline_1s"
+    #"wss://stream.binance.com/ws/btcusdt@kline_1s",
 
     def on_message(self, ws, message):
         data = json.loads(message)
