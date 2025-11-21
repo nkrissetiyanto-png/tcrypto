fba18e8c9a7c9e66c4f7dfbb22d102b98861c1ca
nkrissetiyanto-png <nkrissetiyanto@me.com>
2025-11-22 00:55:03 +0700

    Update orderbook.py

--- orderbook.py
+++ orderbook.py
@@ -2,22 +2,22 @@ import requests
 import pandas as pd
 
 class OrderbookClient:
-    def __init__(self, symbol="btcusdt"):
-        self.url = f"https://api.binance.com/api/v3/depth?symbol={symbol.upper()}&limit=20"
+    def __init__(self, symbol="BTCUSDT"):
+        self.url = f"https://api.binance.com/api/v3/depth?symbol={symbol.upper()}&limit=50"
 
     def get_depth(self):
+        r = requests.get(self.url, timeout=5)
+
         try:
-            raw = requests.get(self.url, timeout=5).json()
-
-            # Fail-safe jika data tidak lengkap
-            if "bids" not in raw or "asks" not in raw:
-                return raw, pd.DataFrame(), pd.DataFrame()
-
-            bids = pd.DataFrame(raw["bids"], columns=["price", "qty"], dtype=float)
-            asks = pd.DataFrame(raw["asks"], columns=["price", "qty"], dtype=float)
-
-            return raw, bids, asks
-
-        except Exception as e:
-            print("Orderbook error:", e)
+            raw = r.json()
+        except:
             return {}, pd.DataFrame(), pd.DataFrame()
+
+        # Jika API diblokir
+        if "bids" not in raw:
+            return raw, pd.DataFrame(), pd.DataFrame()
+
+        bids = pd.DataFrame(raw["bids"], columns=["price", "qty"]).astype(float)
+        asks = pd.DataFrame(raw["asks"], columns=["price", "qty"]).astype(float)
+
+        return raw, bids, asks
